82019295fe56af3f67dd09699f927d16e76d089e
Yaghmafar <Yaghmafar.2006@gmail.com>
2023-12-16 22:24:58 +0330

    build scan page

--- lib/screens/scan_page.dart
+++ lib/screens/scan_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:plant_app/const/constants.dart';
+import 'package:plant_app/screens/qr_overlay.dart';
 
 class ScanPage extends StatefulWidget {
   const ScanPage({super.key});
@@ -10,9 +14,158 @@ class ScanPage extends StatefulWidget {
 class _ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scan Page'),
+      body: Stack(
+        children: [
+          // AppBar
+          Positioned(
+            top: 50.0,
+            left: 20.0,
+            right: 20.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // X Button
+                  child: Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Constants.primaryColor.withOpacity(0.15),
+                    ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Constants.primaryColor,
+                    ),
+                  ),
+                ),
+                // Share Button
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Constants.primaryColor.withOpacity(0.15),
+                  ),
+                  child: const Icon(
+                    Icons.share,
+                    color: Constants.primaryColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 100.0,
+            left: 20.0,
+            right: 20.0,
+            child: SizedBox(
+                width: size.width * 0.8,
+                height: size.height * 0.8,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            PageTransition(
+                              child: const OpenCamera(),
+                              type: PageTransitionType.fade,
+                              duration: const Duration(milliseconds: 500),
+                            ),
+                          );
+                        },
+                        child: Image.asset('assets/images/code-scan.png', height: 130.0),
+                      ),
+                      const SizedBox(height: 25.0),
+                      Text(
+                        'برای اسکن گیاه، کلیک کنید',
+                        style: TextStyle(
+                          color: Constants.primaryColor.withOpacity(0.8),
+                          fontFamily: 'Lalezar',
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OpenCamera extends StatefulWidget {
+  const OpenCamera({super.key});
+
+  @override
+  State<OpenCamera> createState() => _OpenCameraState();
+}
+
+class _OpenCameraState extends State<OpenCamera> {
+  @override
+  Widget build(BuildContext context) {
+    MobileScannerController cameraController = MobileScannerController();
+    return Scaffold(
+      body: Stack(
+        children: [
+          MobileScanner(
+            controller: cameraController,
+            onDetect: (capture) {},
+          ),
+          const QRScannerOverlay(overlayColor: Colors.black26),
+          Positioned(
+            top: 50.0,
+            left: 20.0,
+            right: 20.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  // X Button
+                  child: Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      color: Constants.primaryColor.withOpacity(0.15),
+                    ),
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                // Like Button
+                Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    color: Constants.primaryColor.withOpacity(0.15),
+                  ),
+                  child: const Icon(
+                    Icons.flash_on,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
